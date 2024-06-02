9cac9768f722511893f0c16147e14530a5ee54f6
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-06-03 00:58:33 +0100

    Update 0-hello_route.py\

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """ returns Hello HBNB!"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
